3cf2605b3976f1573dcc389dee1f6674a855fc64
Emerick Bosch <emerick.bosch@gmail.com>
2024-04-02 21:28:33 +0000

    Create a module for outputting to the VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 static HELLO: &[u8] = b"Hello World!";
@@ -12,20 +14,7 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            let line_offset: isize = 160 * 2;
-            let char_offset_within_line: isize = i as isize * 2;
-            let color_offset_within_line: isize = i as isize * 2 + 1;
-            let char_offset = char_offset_within_line + line_offset;
-            let color_offset = color_offset_within_line + line_offset;
-
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
