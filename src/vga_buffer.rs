596fd59c678279eac31564b53908624502d60e73
Emerick Bosch <emerick.bosch@gmail.com>
2024-04-03 15:40:01 +0000

    Implement newline and clearrow

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -83,7 +83,28 @@ impl Writer {
         }
     }
 
-    fn new_line(&mut self) {}
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            // omit first row because this moves offscreen
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_positiion = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
 
     pub fn write_string(&mut self, s: &str) {
         for byte in s.bytes() {
@@ -113,5 +134,5 @@ pub fn print_something() {
 
     writer.write_byte(b'H');
     writer.write_string("ello");
-    write!(writer, "The numbers are {} and {}", 42, 1.0 / 3.0).unwrap();
+    write!(writer, "The numbers are\n{} and\n{}", 42, 1.0 / 3.0).unwrap();
 }
