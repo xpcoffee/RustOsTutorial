55581a9112bd422c2cb13543d03bbc4ebb0afb3b
soltybeans <75095281+soltybeans@users.noreply.github.com>
2024-04-05 16:23:55 +0200

    Test harness set up

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
+mod serial;
 
 use core::panic::PanicInfo;
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info);
@@ -12,10 +17,54 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+#[cfg(test)]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    serial_println!("[FAILED]\n");
+    serial_println!("Error: {}\n", _info);
+    exit_qemu(QemuExitCode::Failed);
+    loop{}
+}
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello, again!");
     println!("A new number:\n{}", 42);
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]){
+    serial_println!("Running {} amount of tests",tests.len());
+    for test in tests {
+        test()
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+#[test_case]
+fn trivial_assertion(){
+    serial_print!("Trivial assertion...");
+    assert_eq!(0, 0);
+    serial_println!("[OK]");
 }
